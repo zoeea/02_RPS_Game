f2cefc384a8e9f127bd66d69fe37d5adff01fc5f
zoeea <99296464+zoeea@users.noreply.github.com>
2022-03-11 09:47:15 +1300

    checkpoint is nearby.

--- 03_RPS_round_mechanics_v2.py
+++ 03_RPS_round_mechanics_v2.py
@@ -15,4 +15,46 @@ def check_rounds():
                     print(round_error)
                     continue
 
-            except
+            except ValueError:
+                print(round_error)
+                continue
+
+        return response
+
+
+# Main routine goes here....
+
+rounds_played = 0
+choose_instructions = "Please choose rock (r), paper " \
+                      "(p) or scissors (s)"
+
+# Ask user for # of rounds, <enter> for infinite mode
+rounds = check_rounds()
+
+end_game = "no"
+while end_game == "no":
+
+    # Rounds Heading
+    print()
+    if rounds == "":
+        heading = "Continuous Mode:"
+        "Round {}".format(rounds_played + 1)
+
+    else:
+        heading = "Rounds {} of {}".format(rounds_played + 1, rounds)
+
+    print(heading)
+    choose = input("{} or 'xxx' to "
+                   "end: ".format (choose_instructions))
+
+    # end game if exit code is typed
+    if choose == "xxx":
+        break
+
+    # rest of loop / game
+    print("You choose {}".format(choose))
+
+    rounds_played +=1
+
+print("Thank you for playing")
+
