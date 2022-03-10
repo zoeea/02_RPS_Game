56f00379b6850f8b29cb6c760dc4e03c175a1950
zoeea <99296464+zoeea@users.noreply.github.com>
2022-03-11 09:52:30 +1300

    building the base

--- 01_RPS_Base_component.py
+++ 01_RPS_Base_component.py
@@ -1,6 +1,25 @@
 import random
 
 # Functions go here
+def check_rounds():
+    while True:
+        response = input("How many rounds: ")
+
+        round_error = "Please type either <enter> " \
+                      "or an integer that is more than 0 "
+        if response != "":
+            try:
+                response = int(response)
+
+                if response < 1:
+                    print(round_error)
+                    continue
+
+            except ValueError:
+                print(round_error)
+                continue
+
+        return response
 
 
 # Main routine goes here
